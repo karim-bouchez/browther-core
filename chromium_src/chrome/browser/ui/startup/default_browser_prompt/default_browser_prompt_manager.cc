77c50ca35efc102bf83ac50e590636ad61400d92
karim <bouchez.karim@gmail.com>
2026-06-26 12:13:49 +0100

    Réactive le prompt 'navigateur par défaut' type Chrome (neutralise l'override Brave)

--- chromium_src/chrome/browser/ui/startup/default_browser_prompt/default_browser_prompt_manager.cc
+++ chromium_src/chrome/browser/ui/startup/default_browser_prompt/default_browser_prompt_manager.cc
@@ -1,18 +1,19 @@
-/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+/* Copyright (c) 2026 dev&din. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "chrome/browser/ui/startup/default_browser_prompt/default_browser_prompt_manager.h"
-
-// Don't display the default browser prompt menu item
-#define show_app_menu_item_      \
-  if (show) {                    \
-    show_app_menu_item_ = false; \
-    return;                      \
-  }                              \
-  show_app_menu_item_
+// Browther : on RÉ-ACTIVE le prompt "définir Browther par défaut" (type Chrome).
+// Brave neutralisait l'app-menu item (forçait `show_app_menu_item_ = false`),
+// supprimant tout rappel pour l'utilisateur. On laisse passer le comportement
+// Chromium upstream : chip dans le menu ⋮ + cooldown natif
+// (DefaultBrowserPromptManager::ShouldShowPrompts : ré-affichage après 21 jours
+// si refusé, max 5 fois). Piloté par la pref kDefaultBrowserPromptEnabled
+// (déjà enregistrée à `true` dans brave_local_state_prefs.cc). Ne s'affiche pas
+// si Browther est déjà le navigateur par défaut.
+//
+// NB : override volontairement "pass-through" (et non supprimé) pour que tout
+// retour de la suppression côté Brave lors d'un `git merge upstream` ressorte
+// en conflit visible plutôt que de re-désactiver silencieusement le prompt.
 
 #include <chrome/browser/ui/startup/default_browser_prompt/default_browser_prompt_manager.cc>
-
-#undef show_app_menu_item_
